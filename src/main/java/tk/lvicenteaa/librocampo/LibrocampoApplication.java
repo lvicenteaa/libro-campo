5e018918a0838ccde0376880c23eebf09bf62273
Luis Vicente Albarracin <albarracinluisvicente@gmail.com>
2022-12-19 09:56:13 -0500

    inicio del proyecto

---
+++ src/main/java/tk/lvicenteaa/librocampo/LibrocampoApplication.java
@@ -0,0 +1,13 @@
+package tk.lvicenteaa.librocampo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrocampoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrocampoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tk/lvicenteaa/librocampo/LibrocampoApplicationTests.java
@@ -0,0 +1,13 @@
+package tk.lvicenteaa.librocampo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrocampoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
